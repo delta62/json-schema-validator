c1fe562154f8d1c8a1749a54b7d694c192eae1ed
Sam Noedel <sam.noedel@gmail.com>
2019-03-25 18:57:34 -0700

    Add integer validation

--- src/generators/number.ts
+++ src/generators/number.ts
@@ -2,19 +2,25 @@ import { Validator, addConstraint, validator, constraint } from './validator'
 import { ObjectSchema } from '../schema'
 
 const constraintHash: Partial<Record<keyof ObjectSchema, string>> = {
-    minimum:          'min',
-    maximum:          'max',
-    exclusiveMinimum: 'greater',
-    exclusiveMaximum: 'less',
-    multipleOf:       'multiple'
+  minimum:          'min',
+  maximum:          'max',
+  exclusiveMinimum: 'greater',
+  exclusiveMaximum: 'less',
+  multipleOf:       'multiple'
 }
 
 export default function generateNumberSchema(schema: ObjectSchema): Validator {
-    return Object.entries(constraintHash).reduce((acc, [ jsonName, joiName ]) => {
-        if (schema.hasOwnProperty(jsonName)) {
-            let value: number = schema[jsonName as keyof ObjectSchema]
-            return addConstraint(acc, constraint(joiName!, value))
-        }
-        return acc
-    }, validator('number'))
+  let v =  Object.entries(constraintHash).reduce((acc, [ jsonName, joiName ]) => {
+    if (schema.hasOwnProperty(jsonName)) {
+      let value: number = schema[jsonName as keyof ObjectSchema]
+      return addConstraint(acc, constraint(joiName!, value))
+    }
+    return acc
+  }, validator('number'))
+
+  if (schema.type === 'integer') {
+    v = addConstraint(v, constraint('integer'))
+  }
+
+  return v
 }
