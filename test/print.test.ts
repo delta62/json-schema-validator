ae940e4803f843fafd857b967115d04bdd7b09b1
Sam Noedel <sam.noedel@gmail.com>
2019-03-27 19:06:17 -0700

    Add more printer tests

--- test/print.test.ts
+++ test/print.test.ts
@@ -1,8 +1,13 @@
 import printValidator from '../src/print'
 
 test('prints an empty validator', () => {
-  let result = printValidator('joi', { name: 'any', params: [], constraints: [] })
+  let result = printValidator('joi', { name: 'any', params: [ ], constraints: [ ] })
   expect(result).toEqual('joi.any()')
+})
+
+test('prints using a given joi name', () => {
+  let result = printValidator('cheese', { name: 'any', params: [ ], constraints: [ ] })
+  expect(result).toEqual('cheese.any()')
 })
 
 test('prints a validator with a constraint', () => {
@@ -14,4 +19,11 @@ test('prints a validator with a constraint', () => {
     ]
   })
   expect(result).toEqual('joi.array().unique()')
+})
+
+test('prints a constraint with an argument', () => {
+  let result = printValidator('joi', { name: 'string', params: [ ], constraints: [
+    { name: 'min', params: [ 42 ] }
+  ] })
+  expect(result).toEqual('joi.string().min(42)')
 })
