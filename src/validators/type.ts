69cb60e7ca604f99157f8c206318476136f02156
Sam Noedel <sam.noedel@gmail.com>
2019-04-10 06:55:13 -0700

    Fix edge case for integer type check

--- src/validators/type.ts
+++ src/validators/type.ts
@@ -1,11 +1,15 @@
-export default function typeValidator(typ: string | string[], instance: any): boolean {
+import { SchemaType } from '../schema'
+
+export default function typeValidator(
+  typ: SchemaType | SchemaType[], instance: any
+): boolean {
   if (typeof typ === 'string') {
     typ = [ typ ]
   }
   return typ.some(t => isScalarType(t, instance))
 }
 
-function isScalarType(expected: string, actual: any): boolean {
+function isScalarType(expected: SchemaType, actual: any): boolean {
   switch (expected) {
     case 'boolean':
       return typeof actual === 'boolean'
@@ -14,7 +18,9 @@ function isScalarType(expected: string, actual: any): boolean {
     case 'number':
       return typeof actual === 'number' && actual !== Infinity
     case 'integer':
-      return typeof actual === 'number' && Math.floor(actual) === actual
+      return typeof actual === 'number'
+        && actual !== Infinity
+        && Math.floor(actual) === actual
     case 'object':
       return typeof actual === 'object' && actual !== null
     case 'array':
