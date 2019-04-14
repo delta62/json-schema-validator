a57460a6a3bb34a74e23307052130b27d9b6e5d0
Sam Noedel <sam.noedel@gmail.com>
2019-04-14 14:22:51 -0700

    Add min/max tests

---
+++ test/validators/maximum.test.ts
@@ -0,0 +1,30 @@
+import maximumValidator from '../../src/validators/maximum'
+
+([
+  [ 42, 10 ],
+  [ 42, 42 ],
+  [ 13.01, 12.34 ],
+  [ 12.34, 12.34 ],
+  [ 0, null ],
+  [ 0, false ],
+  [ 0, 'foo' ],
+  [ 0, [ ] ],
+  [ 0, { } ]
+] as [ number, any][]).forEach(([ max, actual ]) => successCase(max, actual));
+
+[
+  [ 10, 11 ],
+  [ 10.01, 10.02 ]
+].forEach(([ max, actual ]) => failureCase(max, actual))
+
+function successCase(max: number, actual: any) {
+  test(`validates ${actual}`, () => {
+    expect(maximumValidator(max, actual as never)).toBe(true)
+  })
+}
+
+function failureCase(max: number, actual: any) {
+  test(`fails to validate ${actual} <= ${max}`, () => {
+    expect(maximumValidator(max, actual as never)).toBe(false)
+  })
+}
---
+++ test/validators/minimum.test.ts
@@ -0,0 +1,30 @@
+import minimumValidator from '../../src/validators/minimum'
+
+([
+  [ 42, 42 ],
+  [ 10, 11 ],
+  [ 10.01, 10.02 ],
+  [ 12.34, 12.34 ],
+  [ 0, null ],
+  [ 0, false ],
+  [ 0, 'foo' ],
+  [ 0, [ ] ],
+  [ 0, { } ]
+] as [ number, any][]).forEach(([ min, actual ]) => successCase(min, actual));
+
+[
+  [ 42, 10 ],
+  [ 13.01, 12.34 ]
+].forEach(([ min, actual ]) => failureCase(min, actual))
+
+function successCase(min: number, actual: any) {
+  test(`validates ${actual}`, () => {
+    expect(minimumValidator(min, actual as never)).toBe(true)
+  })
+}
+
+function failureCase(min: number, actual: any) {
+  test(`fails to validate ${actual} >= ${min}`, () => {
+    expect(minimumValidator(min, actual as never)).toBe(false)
+  })
+}
