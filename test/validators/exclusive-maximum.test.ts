6418ce4168746a57a6d5d92b5ae8c4eb8b3683f8
Sam Noedel <sam.noedel@gmail.com>
2019-04-14 15:10:49 -0700

    Add tests for exclusive min/max

---
+++ test/validators/exclusive-maximum.test.ts
@@ -0,0 +1,30 @@
+import exclusiveMaximumValidator from '../../src/validators/exclusive-maximum'
+
+([
+  [ 42, 10 ],
+  [ 13.01, 12.34 ],
+  [ 0, null ],
+  [ 0, false ],
+  [ 0, 'foo' ],
+  [ 0, [ ] ],
+  [ 0, { } ]
+] as [ number, any][]).forEach(([ max, actual ]) => successCase(max, actual));
+
+[
+  [ 42, 42 ],
+  [ 12.34, 12.34 ],
+  [ 10, 11 ],
+  [ 10.01, 10.02 ]
+].forEach(([ max, actual ]) => failureCase(max, actual))
+
+function successCase(max: number, actual: any) {
+  test(`validates ${actual}`, () => {
+    expect(exclusiveMaximumValidator(max, actual as never)).toBe(true)
+  })
+}
+
+function failureCase(max: number, actual: any) {
+  test(`fails to validate ${actual} < ${max}`, () => {
+    expect(exclusiveMaximumValidator(max, actual as never)).toBe(false)
+  })
+}
